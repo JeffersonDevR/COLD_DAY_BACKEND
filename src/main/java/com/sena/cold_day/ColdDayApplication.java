390f15314742cadd20c4bcd2253ebf9c4c920d2f
JeffersonDevR <jeffers1917@hotmail.com>
2026-09-05 23:14:45 -0500

    chore: baseline scaffold (Spring Boot 4.1.1 + Spring Modulith 2.1.1)

---
+++ src/main/java/com/sena/cold_day/ColdDayApplication.java
@@ -0,0 +1,13 @@
+package com.sena.cold_day;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColdDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColdDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sena/cold_day/ColdDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.cold_day;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColdDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
